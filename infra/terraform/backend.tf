625160097e3d1d46514dd4a7f4fae0412245233c
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-02-16 21:57:38 +0100

    move out code for verifying s3 bucket where tf state is stored to separate file as per clean code principles for terraform

---
+++ infra/terraform/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "teleco-churn-terraform-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    profile        = "teleco-churn-terraform"
+  }
+}
