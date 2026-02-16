6b6aecf6430d939c39233369166483ffbfa85c84
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-02-16 15:21:38 +0100

    create aws terraform provider and initialize terraform

---
+++ infra/terraform/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "teleco-churn-terraform-state"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+    profile        = "teleco-churn-terraform"
+  }
+}
+
+provider "aws" {
+    alias = "eu-central"
+    region = "eu-central-1"
+    profile = "teleco-churn-terraform"
+}
