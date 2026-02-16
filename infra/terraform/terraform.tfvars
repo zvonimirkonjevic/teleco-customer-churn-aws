d4e9b8d175d69e0ab871dde13a44ed190ded1a48
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-02-16 15:56:22 +0100

    create variable for model data uri

--- infra/terraform/terraform.tfvars
+++ infra/terraform/terraform.tfvars
@@ -1,1 +1,2 @@
 default_region = "eu-central-1"
+model_data_uri = "s3://zvonimir-teleco-customer-churn/models/xgboost/xgboost-260216-0910-011-9f99f443/output/model.tar.gz"
--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -2,3 +2,8 @@ variable "default_region" {
     type        = string
     description = "Default region where your infrastructure is in"
 }
+
+variable "model_data_uri" {
+    type        = string
+    description = "URI of the model data in S3"
+}
