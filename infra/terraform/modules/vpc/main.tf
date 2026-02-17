4c5b058c01f3d1eb8446f2f63a46c28f145de874
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-02-17 15:59:12 +0100

    add subnets to defined availability zones and add tags for resources

--- infra/terraform/modules/vpc/main.tf
+++ infra/terraform/modules/vpc/main.tf
@@ -9,15 +9,28 @@ resource "aws_vpc" "teleco-customer-churn-vpc" {
 }
 
 resource "aws_subnet" "teleco-customer-churn-public-subnet" {
+    count = length(var.availability_zones)
     vpc_id = aws_vpc.teleco-customer-churn-vpc.id
     cidr_block = var.public_subnet_cidr_block
+    availability_zone = var.availability_zones[count.index]
+    tags = {
+        Name = "${var.name}-public-subnet-${count.index + 1}"
+    }
 }
 
 resource "aws_subnet" "teleco-customer-churn-private-subnet" {
+    count = length(var.availability_zones)
     vpc_id = aws_vpc.teleco-customer-churn-vpc.id
     cidr_block = var.private_subnet_cidr_block
+    availability_zone = var.availability_zones[count.index]
+    tags = {
+        Name = "${var.name}-private-subnet-${count.index + 1}"
+    }
 }
 
 resource "aws_internet_gateway" "teleco-customer-churn-internet-gateway" {
     vpc_id = aws_vpc.teleco-customer-churn-vpc.id
+    tags = {
+        Name = "${var.name}-igw"
+    }
 }
