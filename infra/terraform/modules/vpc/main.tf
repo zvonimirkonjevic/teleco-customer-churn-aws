6ceace64467e3e8274245fa680532ba46b246eb8
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-02-17 16:05:18 +0100

    create route table for public subnet and associate it with internet gateway

--- infra/terraform/modules/vpc/main.tf
+++ infra/terraform/modules/vpc/main.tf
@@ -34,3 +34,21 @@ resource "aws_internet_gateway" "teleco-customer-churn-internet-gateway" {
         Name = "${var.name}-igw"
     }
 }
+
+resource "aws_route_table" "teleco-customer-churn-public-route-table" {
+    count = length(var.availability_zones)
+    vpc_id = aws_vpc.teleco-customer-churn-vpc.id
+    route = {
+        cidr_block = var.public_route_table_cidr_block
+        gateway_id = aws_internet_gateway.teleco-customer-churn-internet-gateway.id
+    }
+    tags = {
+        Name = "${var.name}-public-rt-${count.index + 1}"
+    }
+}
+
+resource "aws_route_table_association" "teleco-customer-churn-public-route-table-association" {
+    count = length(var.availability_zones)
+    subnet_id = aws_subnet.teleco-customer-churn-public-subnet[count.index].id
+    route_table_id = aws_route_table.teleco-customer-churn-public-route-table[count.index].id
+}
