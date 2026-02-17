589f66707c6e70fb91561798ccd3c8f051f9980d
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-02-17 16:16:22 +0100

    update sagemaker module var references and implement vpc module

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -2,12 +2,24 @@ module "iam" {
     source = "./modules/iam"
 }
 
+module "vpc" {
+    source = "./modules/vpc"
+
+    name = var.vpc_name
+    cidr_block = var.vpc_cidr_block
+    instance_tenancy = var.vpc_instance_tenancy
+    availability_zones = var.availability_zones
+    public_subnet_cidr_block = var.public_subnet_cidr_block
+    private_subnet_cidr_block = var.private_subnet_cidr_block
+    public_route_table_cidr_block = var.public_route_table_cidr_block
+}
+
 module "sagemaker-serverless-endpoint" {
     source = "./modules/sagemaker-endpoint"
     
-    default_region = var.default_region
+    default_region = var.sagemaker_endpoint_default_region
     iam_role_arn = module.iam.sagemaker_execution_role_iam_arn
-    model_data_uri = var.model_data_uri
+    model_data_uri = var.sagemaker_model_data_uri
     max_concurrency = var.sagemaker_max_concurrency
     memory_size_in_mb = var.sagemaker_memory_size_in_mb
 }
