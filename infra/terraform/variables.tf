9da80da05dcb950475801406fffbb2953a25cdc2
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-02-17 16:15:34 +0100

    refactor some sagemaker variable names and define vpc variables

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,9 +1,10 @@
-variable "default_region" {
+# SageMaker endpoint configuration variables
+variable "sagemaker_endpoint_default_region" {
     description = "AWS region for resource deployment"
     type = string
 }
 
-variable "model_data_uri" {
+variable "sagemaker_model_data_uri" {
     description = "S3 URI path to the trained machine learning model data"
     type = string
 }
@@ -16,4 +17,41 @@ variable "sagemaker_max_concurrency" {
 variable "sagemaker_memory_size_in_mb" {
     description = "Memory size in MB for the SageMaker endpoint"
     type = number
+}
+
+# VPC configuration variables
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type = string
+}
+
+variable "vpc_instance_tenancy" {
+  description = "Instance tenancy for the VPC"
+  type = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones for subnets"
+  type = list(string)
+}
+
+variable "public_subnet_cidr_block" {
+  description = "CIDR block for public subnets"
+  type = string
+}
+
+variable "private_subnet_cidr_block" {
+  description = "CIDR block for private subnets"
+  type = string
+}
+
+variable "public_route_table_cidr_block" {
+  description = "CIDR block for public route table"
+  type = string
 }
