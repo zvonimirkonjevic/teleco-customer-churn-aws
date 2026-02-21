d1f1090c14a5c725fb908fc9b68310a57c5b6ad4
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-02-21 12:56:57 +0100

    add variables used in security group module

---
+++ infra/terraform/modules/security-group/variables.tf
@@ -0,0 +1,43 @@
+variable "name" {
+    description = "The name of the security group."
+    type        = string  
+}
+
+variable "vpc_id" {
+    description = "The VPC ID for the security group."
+
+    type        = string
+}
+
+variable "vpc_cidr_block" {
+    description = "The VPC CIDR block."
+    type        = string
+}
+
+variable "ingress_rules" {
+    description = "The ingress rules for the security group."
+    type        = list(object({
+        from_port   = number
+        to_port     = number
+        protocol    = string
+        cidr_blocks = optional(list(string), [])
+        security_groups = optional(list(string), [])
+    }))
+    default = []
+}
+
+variable "egress_rules" {
+    description = "The egress rules for the security group."
+    type        = list(object({
+        from_port   = number
+        to_port     = number
+        protocol    = string
+        cidr_blocks = optional(list(string), [])
+    }))
+    default = [ {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    } ]
+}
