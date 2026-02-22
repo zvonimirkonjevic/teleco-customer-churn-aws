61760554b77f7d31aa39a42057306218de5c5df4
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-02-22 20:36:30 +0100

    add endpoint arn to output of the module

--- infra/terraform/modules/sagemaker-endpoint/outputs.tf
+++ infra/terraform/modules/sagemaker-endpoint/outputs.tf
@@ -2,3 +2,8 @@ output "sagemaker_endpoint_name" {
   description = "The name of the SageMaker endpoint."
   value       = aws_sagemaker_endpoint.xgboost_endpoint.name
 }
+
+output "sagemaker_endpoint_arn" {
+  description = "The ARN of the SageMaker endpoint."
+  value       = aws_sagemaker_endpoint.xgboost_endpoint.arn
+}
